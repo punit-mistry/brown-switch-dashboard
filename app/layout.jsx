f4acea50190619a702402780c6da6a21a767b2d8
Punit <punitmistr@gmail.com>
2025-01-08 07:14:45 +0530

    fixed the issue of the layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,21 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from 'next/font/google'
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Dashboard',
+  description: 'Order tracking and inventory management',
+}
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className={inter.className}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
